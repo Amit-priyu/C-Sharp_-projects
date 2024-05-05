0cca6f975de9600877d2156370c92958ee68d2c3
Amit-Priyu <priyuocta@gmail.com>
2024-05-05 09:38:53 +0530

    commit

---
+++ Area-Shape/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area_Shape
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ans; float finaresult;
+            Console.WriteLine("Enter the shape for which u  want to calculate the area: 'r' for reatangel , any other for the ciricle ");
+            ans=Console.ReadLine();
+            if (ans == "r")
+            {
+                Console.WriteLine("enter length of rectangle");
+                float len=float.Parse(Console.ReadLine());
+                Console.WriteLine("enter breadth of rectangle");
+                float breadth = float.Parse(Console.ReadLine());
+                finaresult = len * breadth;
+
+            }
+            else
+            {
+                Console.WriteLine("enter radius for radius");
+                float rad=float.Parse(Console.ReadLine());
+                finaresult = (float)Math.PI*rad * rad;
+            }
+            Console.WriteLine("Result : "+ finaresult);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Turn Based Combat/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Turn_Based_Combat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int player = 40; int enemy = 20;
+            int playerattack = 5; int enemyattack = 7;
+            int healamt = 5; 
+            while(player > 0 && enemy > 0)
+            {
+                Console.WriteLine("Player turn");
+                Console.WriteLine("enter 'a' for attack or 'h' for healing ");
+                string choice = Console.ReadLine();
+                if(choice == "a")
+                {
+                    enemy = enemy - playerattack;
+                    Console.WriteLine("player attack enemy and deals "+playerattack +" damage");
+
+                }
+            } 
+        }
+    }
+}
